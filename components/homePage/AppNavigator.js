8fe52dbe98beba0fe56c292f4bddc8d9c96f05ef
t3n1o12 <lvnhat.17it1@vku.udn.vn>
2021-06-06 10:49:25 +0700

    fixbug 0.0.1

--- components/homePage/AppNavigator.js
+++ components/homePage/AppNavigator.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
+import { createStackNavigator } from "@react-navigation/stack"; 
 import Setting from './Setting'
 import LichThi from '../inSettingPage/LichThi'
 import GioiThieu from "../inSettingPage/GioiThieu";
 import DKTC from '../inSettingPage/DKTC'
+import HocPhi from '../inSettingPage/HocPhi'
+import KetQua from "../inSettingPage/KetQua";
 const { Navigator, Screen } = createStackNavigator();
 
 const HomeNavigator = () => (
@@ -15,6 +16,8 @@ const HomeNavigator = () => (
     <Screen name="LichThi" component={LichThi} />
     <Screen name="GioiThieu" component={GioiThieu} />
     <Screen name="DKTC" component={DKTC} />
+    <Screen name="HocPhi" component={HocPhi}/>
+    <Screen name="KetQua" component={KetQua}/>
   </Navigator>
 );
 
